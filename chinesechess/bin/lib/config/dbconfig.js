419bc00475c9b41520c532e80018105b49fa623e
yanghaobo <yangneko@163.com>
2017-09-28 14:34:04 +0800

    2017年9月28日14:32:04

---
+++ chinesechess/bin/lib/config/dbconfig.js
@@ -0,0 +1,13 @@
+/**
+ * 数据库配置
+ * @type {{host: string, post: string, user: string, password: string, database: string}}
+ */
+var dbconfig = {
+    host:'127.0.0.1',
+    post:'3306',
+    user:'root',
+    password:'123456',
+    database:'chinesechess'
+}
+
+module.exports = dbconfig;
