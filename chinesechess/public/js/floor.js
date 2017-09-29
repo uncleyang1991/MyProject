61c3e05e5964b33ce2690fc4ce04c739bd1719ef
yanghaobo <yangneko@163.com>
2017-09-29 16:44:40 +0800

    2017年9月29日16:44:27 大厅功能基本完成

--- chinesechess/public/js/floor.js
+++ chinesechess/public/js/floor.js
@@ -1,47 +1,154 @@
 $(function(){
-    $('#bulletin').liMarquee({
-        direction: 'left',
-        drag:false
+
+    //加载大厅公告
+    $.ajax({
+        url:'/notice',
+        dataType:'json',
+        success:function(msg){
+            if('system'==msg.type){
+                $("#floor_notice").html('<font color="red">系统公告：'+msg.content+'</font>');
+            }
+            $('#bulletin').liMarquee({
+                direction: 'left',
+                drag:false
+            });
+        }
     });
 
+    //加载房间列表
     $('#room_list').dataTable({
         //自动列宽
-        "bAutoWidth": false,
+        'bAutoWidth': false,
         //加载数据时显示正在加载信息
-        "bProcessing": true,
+        'bProcessing': true,
         //分页
-        "paging": true,
-        "bStateSave" : true,
-        "sScrollY": "360.5px", //支持垂直滚动
+        'paging': true,
+        'bStateSave' : true,
+        'sScrollY': '360.5px', //支持垂直滚动
         //从服务器处理分页
-        "bServerSide": true,
+        'bServerSide': true,
         //查询请求action url
-        "ajax": {
-            "url": "/room_list",
-            "type":"post",
-            "dataSrc": "data"
+        'ajax': {
+            'url': '/room_list',
+            'type':'post',
+            'dataSrc': 'data'
         },
         // 客户端传给服务器的查询参数为sSearch,服务端根据条件查出数据源即可
         //'bFilter':true ,
         //本地搜索
-        "searching": false,
+        'searching': false,
         //每页显示多少条数据
-        "lengthChange":false,
+        'lengthChange':false,
         //每页显示数量：5条记录
-        "iDisplayLength": 10,
-        "columns": [
-            {"data": "no","width":"15%"},
-            {"data": "name","width":"40%"},
-            {"data": "owner","width":"30%"},
-            {"data": "state","width":"15%"}
+        'iDisplayLength': 10,
+        //加载完数据后的回调函数
+        'fnInfoCallback':function(){
+            $('#room_list tbody').on( 'click', 'tr', function () {
+                if('暂时没有房间'==$(this).text()){
+                    return;
+                }
+                $('#room_list tbody tr').removeClass('selected');
+                $(this).addClass('selected');
+            });
+        },
+        'columns': [
+            {'data': 'no','width':'15%'},
+            {'data': 'name','width':'40%'},
+            {'data': 'owner','width':'30%'},
+            {'data': 'state','width':'15%',render:function(data){
+                return data?'<font color="red">对战中</font>':'<font color="green">等待中</font>';
+            }},
+            {'data': 'id',render:function(data){
+                return '<input type="hidden" value="'+data+'"/>';
+            }}
         ],
 
         //语言
-        "language": {
+        'language': {
             url: 'internation/message_zh_CN.txt'
         },
         //排序
-        "sort":false,
-        "aaSorting": []
+        'sort':false,
+        'aaSorting': []
+    });
+
+    //刷新房间按钮事件
+    $('#btn_flush_room').on('click',function(){
+        roomListReload();
+    });
+    //退出大厅按钮事件
+    $('#btn_logout').on('click',function(){
+        $('#modal_logout').modal('show');
+    });
+    //退出大厅模态框确定按钮事件
+    $('#btn_modal_logout_enter').on('click',function(){
+        $.ajax({
+            url:'/logout',
+            success:function(msg){
+                if(msg.success)
+                    window.location.href="/";
+            }
+        });
+    });
+    //创建房间按钮事件
+    $('#btn_createroom').on('click',function(){
+        $('#input_modal_createroom_name').val('');
+        $('#modal_createroom').modal('show');
+    });
+    //创建房间模态框确定按钮事件
+    $('#btn_modal_createroom_enter').on('click',function(){
+        var name = $('#input_modal_createroom_name').val().trim();
+        $.ajax({
+            url:'/createroom',
+            type:'post',
+            data:{roomName:name},
+            success:function(msg){
+                if(msg.success){
+                    roomListReload();
+                    $('#input_modal_createroom_name').val('');
+                    $('#modal_createroom').modal('hide');
+                    //创建成功后直接进入游戏界面
+
+
+                }else{
+                    alert(msg.msg);
+                }
+            }
+        });
+    });
+    //加入房间按钮事件
+    $('#btn_joinroom').on('click',function(){
+        var trs = $('#room_list tbody tr');
+        var select;
+        for(var i=0;i<trs.length;i++){
+            if($(trs[i]).hasClass('selected')){
+                select = $(trs[i]);
+                break;
+            }
+        }
+        if(select){
+            var roomId = $(select.children()[4]).children()[0].value;
+            $.ajax({
+                url:'/joinroom',
+                type:'post',
+                data:{roomId:roomId},
+                success:function(msg){
+                    if(msg.success){
+                        roomListReload();
+                        //加入房间成功
+
+
+                    }else{
+                        alert(msg.msg);
+                    }
+
+                }
+            });
+        }
     });
 });
+
+//房间列表刷新
+function roomListReload(){
+    $('#room_list').DataTable().ajax.reload();
+}
--- chinesechess/routes/index.js
+++ chinesechess/routes/index.js
@@ -18,12 +18,12 @@ router.all('*',function(req,res,next){
 });
 
 //登录页面
-router.get('/', function(req, res, next) {
+router.all('/', function(req, res, next) {
     res.render('login', { title: '登录' });
 });
 
 //登陆请求
-router.post('/login',function(req,res){
+router.all('/login',function(req,res){
     var params = req.body;
     db.query(
         'select id,username,nickname from player where username=\''+params.username+'\' and password=\''+md5(params.password)+'\'',
@@ -40,20 +40,112 @@ router.post('/login',function(req,res){
 });
 
 //跳转大厅
-router.get('/floor',function(req,res){
+router.all('/floor',function(req,res){
     res.render('floor',{title:'大厅'});
 });
 
-router.all('/room_list',function(req,res){
-    var params = req.body;
+//大厅公告
+router.all('/notice',function(req,res){
     db.query(
-        'select a.* from room a join (select id from room order by state,no limit '+params.start+','+params.length+') b on a.id=b.id',
+        'select content,type from notice where state = 1',
         function(result){
-            res.send({"data":result,"iTotalRecords":11});
+            if(result.length==0){
+                res.send({"content":"暂无公告","type":"system"});
+            }else{
+                res.send(result[0]);
+            }
         }
     );
 });
 
+//房间列表
+router.all('/room_list',function(req,res){
+    var params = req.body;
+    db.query(
+        'select a.* from room a join (select id from room order by state limit '+params.start+','+params.length+') b on a.id=b.id order by no',
+        function(result){
+            db.query(
+                'select count(1) as count from room;',
+                function(count_result){
+                    var count = count_result[0].count;
+                    res.send({"data":result,"recordsTotal":count,"recordsFiltered":count});
+                }
+            );
+        }
+    );
+});
 
+//创建房间
+router.all('/createroom',function(req,res){
+    var params = req.body;
+    //回调地狱的开端
+    db.query(
+        'select min(roomno) as minno from roomno where isuse=0',
+        function (minno_result) {
+            var minno = minno_result[0].minno;
+            if(minno){
+                db.query(
+                    'update roomno set isuse=1 where roomno='+minno,
+                    function(update_result){
+                        if(update_result.affectedRows){
+                            db.query(
+                                'insert into room values (' +
+                                '\''+idtools.uuid()+'\',' +
+                                minno+','+
+                                '\''+params.roomName+'\','+
+                                '\''+req.session.player.nickname+'\','+
+                                '0,now())',
+                                function(create_result){
+                                    if(create_result.affectedRows){
+                                        res.send({success:true});
+                                    }else{
+                                        //创建房间失败,error
+                                        res.send({success:false,msg:'错误,创建房间失败!'});
+                                    }
+                                }
+                            );
+                        }else{
+                            //创建房间失败,error
+                            res.send({success:false,msg:'错误,创建房间失败!'});
+                        }
+                    }
+                );
+            }else{
+                //无法创建,房间列表已满
+                res.send({success:false,msg:'目前房间过多,请稍后再创建'});
+            }
+        }
+    );
+});
+
+//加入房间
+router.all('/joinroom',function(req,res){
+    var params = req.body;
+    db.query(
+        'select state from room where id=\''+params.roomId+'\'',
+        function(state_result){
+            if(state_result[0].state==0){
+                db.query(
+                    'update room set state=1 where id=\''+params.roomId+'\'',
+                    function(update_result){
+                        if(update_result.affectedRows){
+                            res.send({success:true});
+                        }else{
+                            res.send({success:false,msg:'错误,加入房间失败!'});
+                        }
+                    }
+                );
+            }else{
+                res.send({success:false,msg:'该房间正在对战中'});
+            }
+        }
+    );
+});
+
+//退出大厅
+router.all('/logout',function(req,res){
+    req.session.player = null;
+    res.send({success:true});
+});
 
 module.exports = router;
