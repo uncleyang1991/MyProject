f51e5a54571b3643b5cf93ac0b47aa53a6bb1a8e
yanghaobo <yanghaobo@boco.com.cn>
2018-03-20 10:25:12 +0800

    2018年3月20日10:24:17 启用MailJob和ResetJob两个任务

--- record/src/main/java/org/uy/record/RecordApplication.java
+++ record/src/main/java/org/uy/record/RecordApplication.java
@@ -3,8 +3,10 @@ package org.uy.record;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 @MapperScan("org.uy.record.dao")
 public class RecordApplication {
 
